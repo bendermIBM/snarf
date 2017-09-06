27a06d4e74ec209f622096185b76bb0b68299f39
benderm <max.bender1@ibm.com>
2017-09-06 11:27:56 -0400

    typo

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
     name='snarf_libvirt',
     packages=['snarf_libvirt'],
-    version='0.3',
+    version='0.4',
     description='A small libvirt helper package',
     author='Max Bender',
     author_email='max.bender1@ibm.com',
     url='https://github.com/bendermIBM/snarf',
-    download_url='hhttps://github.com/bendermIBM/snarf/archive/0.2.tar.gz',
+    download_url='https://github.com/bendermIBM/snarf/archive/0.4.tar.gz',
     keywords=['libvirt'],
     classifiers=[],
 )
