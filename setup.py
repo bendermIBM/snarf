40d107e44d26934725f2142d8c829e2c1588d9a6
benderm <max.bender1@ibm.com>
2017-09-11 13:09:14 -0400

    0.6

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
     name='snarf_libvirt',
     packages=['snarf_libvirt'],
-    version='0.4',
+    version='0.5',
     description='A small libvirt helper package',
     author='Max Bender',
     author_email='max.bender1@ibm.com',
     url='https://github.com/bendermIBM/snarf',
-    download_url='https://github.com/bendermIBM/snarf/archive/0.4.tar.gz',
+    download_url='https://github.com/bendermIBM/snarf/archive/0.5.tar.gz',
     keywords=['libvirt'],
     classifiers=[],
 )
